cb77d4baafdf8e335e30933fa121eb064baecf4d
Franco <franco.soto.z@gmail.com>
2016-03-04 16:33:06 -0300

    First Commit

---
+++ src/models/fixtures/message.js
@@ -0,0 +1,21 @@
+import fixture from 'can-fixture';
+
+const store = fixture.store([{
+  id: 0,
+  description: 'First item',
+  name: 'la concha de tu madre',
+  body: 'puto'
+}, {
+  id: 1,
+  description: 'Second item'
+}]);
+
+fixture({
+  'GET http://chat.donejs.com/api/messages': store.findAll,
+  'GET http://chat.donejs.com/api/messages/{id}': store.findOne,
+  'POST http://chat.donejs.com/api/messages': store.create,
+  'PUT http://chat.donejs.com/api/messages/{id}': store.update,
+  'DELETE http://chat.donejs.com/api/messages/{id}': store.destroy
+});
+
+export default store;
